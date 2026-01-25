5cf63e79618171fbcd5b2f7aa650a1568b2883ad
Mattia Fornari <m.fornari002@studenti.unibs.it>
2026-01-25 11:53:21 +0100

    Updated visualize_lander

--- visualize_lander.py
+++ visualize_lander.py
@@ -1,31 +1,31 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 
-# 1. Carichiamo l'ambiente con la grafica ATTIVA
-# Nota: usa "LunarLanderContinuous-v3" come abbiamo corretto prima
+# 1. Caricamento dell'ambiente con grafica attiva "LunarLanderContinuous-v3"
 env = gym.make("LunarLanderContinuous-v3", render_mode="human")
 
-# 2. Carichiamo il cervello addestrato (assicurati che il file .zip esista!)
+# 2. Caricamento del modello/policy addestrato 
+# (assicurarsi che il file .zip esista - i.e. eseguire prima il training)
 try:
     model = PPO.load("ppo_lunar_lander", device="cpu")
     print("Modello caricato con successo!")
 except:
-    print("ERRORE: Non trovo il file 'ppo_lunar_lander.zip'. Hai fatto il training?")
+    print("ERRORE: 'ppo_lunar_lander.zip'. Eseguire il training se non ancora effettuato!")
     exit()
 
-# 3. Loop di visualizzazione
-episodes = 5  # Quanti atterraggi vuoi vedere
+# 3. Loop di visualizzazione - Inferenza
+episodes = 5  # cambiare per visualiuzzare più atterraggi
 for ep in range(episodes):
-    obs, _ = env.reset()
+    obs, _ = env.reset(seed=42) # imposto seed=42 per debugging e riproducibilità
     done = False
     truncated = False
     print(f"Inizio episodio {ep + 1}...")
     
     while not (done or truncated):
-        # Chiediamo al modello cosa fare
-        action, _ = model.predict(obs, deterministic=True)
+        # Calcolo l'azione "migliore"  disabilitando la stocasticità/esplorazione ==> determinismo
+        action, _ = model.predict(obs, deterministic=True) # 
         
-        # Eseguiamo l'azione (la grafica si aggiorna da sola)
+        # Eseguo l'azione - la grafica si aggiorna autonomamente
         obs, reward, done, truncated, info = env.step(action)
 
 env.close()
